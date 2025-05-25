3a1b5c541d72a1c735bbccde4facc5b005c88150
shakera21 <shanaraakter617@gmail.com>
2025-05-25 23:36:00 +0600

    Update J.cpp

--- sheet2/J.cpp
+++ sheet2/J.cpp
@@ -1,4 +1,4 @@
-1.// Problem name: (I) Palindrome.
+1.// Problem name: (j) Primes from 1 to n .
 2.// Course title: Competitive Programming Sessional
 3.// Course code: CCE-2310
 4.// Course teacher name: Mirza Raquib
