126a60f9d54c48a62cd0ea0fc928dca9c5331c14
shakera21 <shanaraakter617@gmail.com>
2025-05-25 23:09:17 +0600

    Update W.cpp

--- sheet1/W.cpp
+++ sheet1/W.cpp
@@ -1,3 +1,8 @@
+1.// Problem name: (I) Palindrome.
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 5/5/25
 #include<bits/stdc++.h>
 using namespace std;
 int main()
