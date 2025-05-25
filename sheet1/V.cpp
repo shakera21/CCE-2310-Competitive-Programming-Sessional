5d56814f6fde00f01a4c21b22407123026df60e7
shakera21 <shanaraakter617@gmail.com>
2025-05-25 23:08:54 +0600

    Update V.cpp

--- sheet1/V.cpp
+++ sheet1/V.cpp
@@ -1,4 +1,8 @@
-#include<bits/stdc++.h>
+#incl1.// Problem name: (I) Palindrome.
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 5/5/25ude<bits/stdc++.h>
 using namespace std;
 int main()
 {
