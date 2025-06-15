0233dc15428774245e0a22e196d056f29d598bfe
shakera21 <shanaraakter617@gmail.com>
2025-06-15 21:34:35 +0600

    Add files via upload

---
+++ sheet3/sheet3/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    long long n;
+cin>>n;
+long long sum =0;
+for(int i=1;i<=n;i++)
+{
+    long long a;
+    cin>>a,
+   sum=sum+a;
+
+
+}
+        cout<<abs (sum)<<endl;
+
+
+}
