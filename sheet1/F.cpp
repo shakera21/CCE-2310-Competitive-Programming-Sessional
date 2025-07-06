b825c1abf35aa3f042d9885c67687a1143dce903
shakera21 <shanaraakter617@gmail.com>
2025-07-06 20:17:18 +0600

    Update F.cpp

--- sheet1/F.cpp
+++ sheet1/F.cpp
@@ -1,4 +1,4 @@
-1.// Problem name: (I) Palindrome.
+1.// Problem name: (F)Digits Summation .
 2.// Course title: Competitive Programming Sessional
 3.// Course code: CCE-2310
 4.// Course teacher name: Mirza Raquib
