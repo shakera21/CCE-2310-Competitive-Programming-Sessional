543b710c0cdabf5dff82a8158386b6771238b3e5
shakera21 <shanaraakter617@gmail.com>
2025-08-01 20:11:36 +0600

    Update S.cpp

--- sheet1/S.cpp
+++ sheet1/S.cpp
@@ -1,4 +1,4 @@
-1.// Problem name: (I) Palindrome.
+1.// Problem name: (S)Interval .
 2.// Course title: Competitive Programming Sessional
 3.// Course code: CCE-2310
 4.// Course teacher name: Mirza Raquib
